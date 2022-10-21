835d68bd8a05d9e064e4770ffa7e8590f0bdac50
KirillTaE <82940632+KirillTaE@users.noreply.github.com>
2022-10-21 11:40:37 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,30 +2,20 @@ import face_recognition
 import cv2.cv2 as cv
 import numpy as np
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
 
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
-# Get a reference to webcam #0 (the default one)
 video_capture = cv.VideoCapture(0)
 
-# Load a sample picture and learn how to recognize it.
+
 Romanenko_Kirill_image = face_recognition.load_image_file("Romanenko_Kirill.jpg")
 Romanenko_Kirill_face_encoding = face_recognition.face_encodings(Romanenko_Kirill_image)[0]
 
-# Load a second sample picture and learn how to recognize it.
+
 Mironov_Kirill_image = face_recognition.load_image_file("Mironov_Kirill.jpg")
 Mironov_Kirill_face_encoding = face_recognition.face_encodings(Mironov_Kirill_image)[0]
 
 Dvoretskov_Kirill_image = face_recognition.load_image_file("Dvoretskov_Kirill.jpg")
 Dvoretskov_Kirill_face_encoding = face_recognition.face_encodings(Dvoretskov_Kirill_image)[0]
 
-# Create arrays of known face encodings and their names
 known_face_encodings = [
     Romanenko_Kirill_face_encoding,
     Mironov_Kirill_face_encoding,
@@ -45,7 +35,6 @@ known_face_encodings_with_names = [
 ]
 '''
 
-# Initialize some variables
 face_locations = []
 face_encodings = []
 face_names = []
